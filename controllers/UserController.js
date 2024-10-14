8faff47000536a7bfd3478a8683fef3a1d3e53ba
ThaisPio <89753258+ThaisPio@users.noreply.github.com>
2024-10-14 11:06:52 -0300

    Update UserController.js
    Atualizado

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,12 +1,12 @@
 import { insertUser, validateUserLogin } from "../models/User";
 
-
+//Adicionar um novo usuário ao banco de dados
 export const handleAddUser = (name, cpf_email, phone, password, successCallback) => {
     insertUser(name, cpf_email, phone, password, (result) => {
         successCallback(result);
     });
 };
-
+//Valida o login de um usuário - verifica se o CPF/e-mail e senha fornecidos estão corretos.
 export const getUser = (cpf_email, password, successCallback) => {
     validateUserLogin(cpf_email, password, (user) => {
       successCallback(user); // Passa o objeto `user` completo com `id`
